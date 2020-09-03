26640bf96df0327b3eba0c98bf5523448e2f937f
Tống Hoàng Hải <59403368+BOT-MEO@users.noreply.github.com>
2020-09-03 23:36:20 +0700

    Sửa lại màu cho dễ nhìn :))

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -486,21 +486,4 @@ public class Calculator extends JFrame {
 		contentPane.add(textField_ketqua);
 		
 	}
-	private static void addPopup(Component component, final JPopupMenu popup) {
-		component.addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			public void mouseReleased(MouseEvent e) {
-				if (e.isPopupTrigger()) {
-					showMenu(e);
-				}
-			}
-			private void showMenu(MouseEvent e) {
-				popup.show(e.getComponent(), e.getX(), e.getY());
-			}
-		});
-	}
 }
